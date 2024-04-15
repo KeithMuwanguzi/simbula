528c4d7142fb8dcf16a5db6ed48cd30ce2896e59
keith <keithmuwanguzi@gmail.com>
2024-04-15 20:45:07 +0300

    Updated path

--- lib/features/home/presentation/car_pool.dart
+++ lib/features/home/presentation/car_pool.dart
@@ -1,11 +1,15 @@
 import 'dart:async';
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:http/http.dart' as http;
+import 'package:some_ride/core/constants/constants.dart';
+import 'package:geocoding/geocoding.dart';
 
 import 'package:some_ride/features/home/controllers/home_controller.dart';
-import 'package:some_ride/features/home/presentation/search_bar.dart';
 
 class CarPoolHome extends StatefulWidget {
   const CarPoolHome({super.key});
@@ -19,6 +23,52 @@ class CarPoolHome extends StatefulWidget {
 class _CarPoolHomeState extends State<CarPoolHome> {
   final controller = Get.find<HomeController>();
   final Completer<GoogleMapController> mapController = Completer();
+  final TextEditingController _searchController = TextEditingController();
+  void getPolyfromPoints(List<LatLng>? points) {
+    PolylineId id = const PolylineId('route');
+    Polyline polyline = Polyline(
+      polylineId: id,
+      color: Colors.blue,
+      width: 5,
+      points: points!,
+    );
+    setState(() {
+      CarPoolHome.polylines[id] = polyline;
+    });
+  }
+
+  List listofPlaces = [];
+  String groundURL =
+      'https://maps.googleapis.com/maps/api/place/autocomplete/json';
+  String tokenForSession = "keith1";
+
+  void makeSuggestion({required String input}) async {
+    String request =
+        '$groundURL?input=$input&key=$googleApiKey&sessiontoken=$tokenForSession';
+    var response = await http.get(Uri.parse(request));
+    if (response.statusCode == 200) {
+      setState(() {
+        listofPlaces = jsonDecode(response.body)['predictions'];
+      });
+    }
+  }
+
+  void onModify() {
+    makeSuggestion(input: _searchController.text);
+  }
+
+  @override
+  void initState() {
+    _searchController.addListener(() {
+      if (_searchController.text.isEmpty) {
+        setState(() {
+          listofPlaces = [];
+        });
+      }
+      onModify();
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,11 +80,11 @@ class _CarPoolHomeState extends State<CarPoolHome> {
                 ? waitingloadingMap()
                 : loadMap(controller, mapController),
           ),
-          const Positioned(
+          Positioned(
             top: 20.0,
             left: 20.0,
             right: 20.0,
-            child: SearchBarPage(),
+            child: searchField(),
           ),
         ],
       ),
@@ -90,6 +140,84 @@ class _CarPoolHomeState extends State<CarPoolHome> {
             Text('Please wait...'),
           ],
         ),
+      ),
+    );
+  }
+
+  Container searchField() {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 5,
+            spreadRadius: 1,
+          ),
+        ],
+      ),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              const Padding(
+                padding: EdgeInsets.all(15.0),
+                child: Icon(
+                  Icons.search,
+                  color: Colors.grey,
+                ),
+              ),
+              Expanded(
+                child: TextField(
+                  controller: _searchController,
+                  decoration: const InputDecoration(
+                    hintText: 'Search for a location',
+                    border: InputBorder.none,
+                  ),
+                  onSubmitted: (value) {
+                    makeSuggestion(input: value);
+                  },
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 10),
+          listofPlaces.isEmpty
+              ? const SizedBox()
+              : ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: listofPlaces.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(
+                        listofPlaces[index]['description'],
+                        style: GoogleFonts.roboto(
+                          color: Colors.black,
+                          fontSize: 16,
+                        ),
+                      ),
+                      onTap: () async {
+                        List<Location> location = await locationFromAddress(
+                            listofPlaces[index]['description']);
+                        controller.updateDestinationMarker(
+                          LatLng(
+                              location.last.latitude, location.last.longitude),
+                        );
+                        controller.changeDestination(
+                          LatLng(
+                              location.last.latitude, location.last.longitude),
+                        );
+                        controller.getPolylinePoints().then(
+                              (value) => getPolyfromPoints(value),
+                            );
+                        _searchController.clear();
+                        setState(() {});
+                      },
+                    );
+                  },
+                ),
+        ],
       ),
     );
   }
